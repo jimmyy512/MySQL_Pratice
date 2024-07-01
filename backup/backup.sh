c18e6107d888cd8bda4628cf1f88ae56575e9976
Ma jitoo <jimmyy5121@gmail.com>
2024-07-01 19:04:31 +0800

    調整時間地區

--- backup/backup.sh
+++ backup/backup.sh
@@ -6,6 +6,10 @@ mkdir -p /backup/backup_sql
 # 從環境變量中獲取備份間隔時間，默認為86400秒（1天）
 BACKUP_INTERVAL=${BACKUP_INTERVAL:-86400}
 
+# 打印當前系統時間和時區
+echo "Starting backup script at $(date)"
+echo "Current system timezone: $(date +'%Z %z')"
+
 while true; do
   TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
   mysqldump -h mysql -u root -prootpassword mydatabase > /backup/backup_sql/backup_$TIMESTAMP.sql
